ae8e689f51a9f91de53e41657f1a1056e6c046ac
Nicole Aptekar <nicoletbn@gmail.com>
2013-02-09 11:44:55 -0800

    added all the things

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Suggestoreddit::Application.config.secret_token = '7eceaba152893b5e11103500260501d09671e6d89e44bbee3a80ef8a5ca21672dfda81930d620de032d1cd71b3a07de0a2e3f0f5eb7010220e13c8a38585cd98'
